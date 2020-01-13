7026b72b7ffe9913b470e3592181ee2c562d81c0
Jaxel Rojas <jaxelr@gmail.com>
2020-01-13 10:29:48 -0400

    feat: Initial version

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace Consola
+{
+    internal class Program
+    {
+        private static void Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Private member is invoked from .Execute<Program>")]
+        private void OnExecute()
+        {
+            Console.WriteLine($"Hello world!");
+
+            if (System.Diagnostics.Debugger.IsAttached)
+                Console.Read();
+        }
+    }
+}
