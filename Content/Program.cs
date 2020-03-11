984342d8b532b8f2874ee90b3a1a555e9ceefaa1
jaxelr <jaxelr@gmail.com>
2020-03-11 11:35:51 -0400

    feat: added serilog for logging purposes

--- Content/Program.cs
+++ Content/Program.cs
@@ -1,18 +1,29 @@
 ﻿using System;
 using McMaster.Extensions.CommandLineUtils;
 using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace Consola
 {
     internal class Program
     {
+        internal const string EnvironmentVariable = "ASPNETCORE_ENVIRONMENT";
+
         private static IConfiguration configuration { get; set; }
         private static Settings Settings { get; set; }
+        private static string env => Environment.GetEnvironmentVariable(EnvironmentVariable);
 
         private static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                                .MinimumLevel.Information()
+                                .Enrich.FromLogContext()
+                                .WriteTo.Console()
+                                .CreateLogger();
+
             configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings{env}.json", true, true)
                 .Build();
 
             //Extract the Settings info from the appsettings config.
@@ -25,11 +36,20 @@ namespace Consola
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Private member is invoked from .Execute<Program>")]
         private void OnExecute()
         {
-            Console.WriteLine("Hello world!");
-            Console.WriteLine($"{Settings.Example}");
+            try
+            {
+                Log.Logger.Information($"Environment used: {env}");
+                Log.Logger.Information("Hello world!");
+                Log.Logger.Information($"{Settings.Example}");
 
-            if (System.Diagnostics.Debugger.IsAttached)
-                Console.Read();
+                if (System.Diagnostics.Debugger.IsAttached)
+                    Console.Read();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+                Environment.Exit(1);
+            }
         }
     }
 }
